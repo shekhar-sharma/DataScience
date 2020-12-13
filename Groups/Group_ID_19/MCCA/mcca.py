b17ac2bcb05e8bf39931f5d3dea1f6e48a96c164
Mansi Verma <58826583+mansi-08@users.noreply.github.com>
2020-12-14 02:39:13 +0530

    Update mcca.py

--- Groups/Group_ID_19/MCCA/mcca.py
+++ Groups/Group_ID_19/MCCA/mcca.py
@@ -14,10 +14,11 @@ class MCCA:
         self.C = [[]]  #covariance matix 
         self.w_list = []  # list of projections
         self.score_list = []
+        self.sum_dimen = []
         
     #To normalize data so that mean=0 and std dev=1
     def normalize(self,X):
-        return StandardScale.fit_tranform(X)
+        return StandardScaler().fit_transform(X)
     
     #To find the covariance matrix containing
     #both within view and between view covariance
@@ -30,12 +31,13 @@ class MCCA:
         #dimention of views
         dimen = [0]*views
         for i in range(views):
-            dimen[i] = len(X[i].T)
+            dimen[i] = len(X_list[i].T)
             
         #sum of dimention till individual view
-        sum_dimen = [0]*views
+        sum_dimen = [0]*(views+1)
         for i in range(1,views):
             sum_dimen[i] = sum([x for x in dimen][:i+1])
+        self.sum_dimen=sum_dimen
             
         #cov_mat containing both within view and between view covariance
         C = [[np.array([]) for i in range(views)] for j in range(views)]
@@ -54,12 +56,12 @@ class MCCA:
             
         return C
     
-    def fit(self,X_list):
+    def fit(self,*X_list):
         
         views = len(X_list)
         x_normalize = [self.normalize(x) for x in X_list]
-        C = cov_mat(x_normalize)
-        C=add_reg_param(C)
+        C = self.cov_mat(x_normalize)
+        #C=add_reg_param(C)
         
         #Constructing A(left) and B(right) matrix of GEP(generalized eigen value problem) 
         A_rows = [np.hstack([np.zeros_like(C[i][j]) if i == j else C[i][j] for j in range(views)])for i in range(views)]
@@ -69,32 +71,32 @@ class MCCA:
         
         #calculating eigen value and eigen vector 
         eig_vals,eig_vecs = lin.eig(A,B)
-        w_list = [eigvecs[start:end] for start, end in zip(dimen[0:-1], dimen[1:])]
+        w_list = [eig_vecs[start:end] for start, end in zip(self.sum_dimen[0:-1], self.sum_dimen[1:])]
         self.w_list = w_list
         self.views = views
         self.C = C
         
-    def tranform(self,X_list):
+    def transform(self,*X_list):
         views = len(X_list)
         X_normalize = [self.normalize(x) for x in X_list]
         i=0
         for X,W in zip(X_normalize,self.w_list):
             score_list[i]=np.dot(X,W)
             i=i+1
-        self.score_lits=score_list
+        self.score_list=score_list
         return score_list
                        
-    def fit_tranform(self,X_list):
+    def fit_transform(self,X_list):
         self.fit(X_list)
         self.tranform(X_list)
                        
                        
-        
-def main():
-    a = np.random.rand(5,5)
-    b = np.random.rand(5,6)
-    c = np.random.rand(5,7)
-    d = np.random.rand(5,8)
-    mcca = MCCA()
-    mcca.fit(a,b,c,d)
-    
+
+a = np.random.rand(5,5)
+b = np.random.rand(5,6)
+c = np.random.rand(5,7)
+d = np.random.rand(5,8)
+mcca = MCCA()
+mcca.fit(a,b,c,d)
+res=mcca.transform(a,b,c,d)
+print(res)  
