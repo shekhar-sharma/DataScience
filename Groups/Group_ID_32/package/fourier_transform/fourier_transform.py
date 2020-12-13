d6f8d21da57390306efda2f287a588eb8ae847e4
prateek-narsinghani <36529094+prateek-narsinghani@users.noreply.github.com>
2020-12-14 02:41:51 +0530

    Update fourier_transform.py

--- Groups/Group_ID_32/package/fourier_transform/fourier_transform.py
+++ Groups/Group_ID_32/package/fourier_transform/fourier_transform.py
@@ -24,15 +24,3 @@ def fourier_transform(img):
   the log of it'''
   magnitude_spectrum = 20*np.log(np.abs(fshift))
   return magnitude_spectrum
-
-'''Example
-
-img = cv2.imread("/content/a_0.png",0)
-magnitude_spectrum = fourier_transform(img)
-
-plt.subplot(121),plt.imshow(img, cmap = 'gray')
-plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
-plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
-plt.show()
-'''
