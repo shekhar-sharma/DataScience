05af4e8cb0ae08e9a86d813c14dfa4b4f3f4ffce
adityaasharma27 <42903231+adityaasharma27@users.noreply.github.com>
2020-12-15 10:49:43 +0530

    Delete code.py

--- Groups/Group_ID_40/code.py
+++
@@ -1,8 +0,0 @@
-from vcca import fit
-import torch
-from torch.autograd import Variable
-
-x_view = Variable(torch.randn(10000, 100))
-y_view = Variable(torch.randn(10000, 100))
-epochs=1
-fit(x_view,y_view,10,100,epochs)
