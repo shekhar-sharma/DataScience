1640056bf3f1d3011310d8613a1c10b869a8b765
Gunjan Pandey <42100021+gunjan203144@users.noreply.github.com>
2020-12-16 20:03:27 +0530

    Update test.py

--- Groups/Group_ID_38/test.py
+++ Groups/Group_ID_38/test.py
@@ -5,4 +5,4 @@ from sklearn.preprocessing import StandardScaler
 
 X=[np.random.randn(10,10) for i in range(10)]
 reduced1=l21cca.l21_cca(X,5)
-reduced2=l21cca.l21_cca(X,10,20,5,5)
+reduced2=l21cca.l21_cca(X,10,20)
