84c49c929f17c37ac9a31e9c586d2da1d9b7c010
Gunjan Pandey <42100021+gunjan203144@users.noreply.github.com>
2020-12-19 18:38:49 +0530

    Update test.py

--- Groups/Group_ID_38/test.py
+++ Groups/Group_ID_38/test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import l21cca
 
-X=[np.random.randn(10,10) for i in range(10)]
+X=[np.random.randn(50,50) for i in range(10)]
 reduced1=l21cca.l21_cca(X,5)
 reduced2=l21cca.l21_cca(X,10,20)
