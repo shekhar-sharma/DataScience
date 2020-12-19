853b90bbb1f70843c590b7571615af94b932b42f
Gunjan Pandey <42100021+gunjan203144@users.noreply.github.com>
2020-12-19 21:16:24 +0530

    Update test.py

--- Groups/Group_ID_38/test.py
+++ Groups/Group_ID_38/test.py
@@ -1,8 +1,12 @@
 import numpy as np
 import l21cca
 
+# X contains list of 10 views
+
 X=[np.random.randn(50,50) for i in range(10)]
 reduced1=l21cca.l21_cca(X,5)
 print(reduced1)
+
+
 reduced2=l21cca.l21_cca(X,10,20)
 print(reduced2)
