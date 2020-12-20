f8463240b108affb4949b2f45bf4671d05d4d013
tanishq719 <47773364+tanishq719@users.noreply.github.com>
2020-12-20 22:09:08 +0530

    Update example.py

--- Groups/Group_ID_4/Resources/example.py
+++ Groups/Group_ID_4/Resources/example.py
@@ -1,7 +1,6 @@
 from RMvFS import RMvFS
 
 r = RMvFS(threshold=0.3,max_iter=14)
-X,y = load_iris(return_X_y=True)
 X1 = [[60, 468], [40, 400], [46, 109], [50, 150], [12, 57]]     #height and width of advertisment images
 X2 = [[1, 1, 0, 0, 0], [1, 1, 1, 0, 0], [0, 0, 0, 1, 1], [1, 1, 0, 0, 0], [1, 0, 0, 1, 1]]    # sparse matrix for various tags in the url of asvertisement
 r.fit(X1,X2,Y=['ad', 'ad', 'notad', 'ad', 'notad'])
