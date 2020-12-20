89b3f88fa6ac087a95154196d441c955269262e7
Mansi Verma <58826583+mansi-08@users.noreply.github.com>
2020-12-20 20:13:12 +0530

    Update mcca.py

--- Groups/Group_ID_19/MCCA/mcca.py
+++ Groups/Group_ID_19/MCCA/mcca.py
@@ -6,24 +6,25 @@ from sklearn.preprocessing import StandardScaler
 
 class MCCA:
     
-    def _init_(self,n_components=2,reg_param=0.01):
+    def __init__(self,n_components=2,reg_param=0.01):
         self.n_components = n_components
         self.reg_param = reg_param
         self.dimen = []
         self.C = [[]]  #covariance matix 
         self.weights = [[]]  # list of projections
-      
+    
         
     #To normalize data so that mean=0 and std dev=1
     def normalize(self,X):
         return StandardScaler().fit_transform(X)
     
     #for calculating dimentions of each view
-    def dimentions(self,X_list)
-    dimen=[0]*views
-    for  i in range(views):
-        dimen[i]=X_list[i].shape[1]
-        self.dimen=dimen
+    def dimentions(self,X_list):
+        views = len(X_list)
+        dimen=[0]*views
+        for  i in range(views):
+            dimen[i]=X_list[i].shape[1]
+        return dimen
     
     #for adding regularization parameter
     def add_reg_param(self,c):
@@ -32,28 +33,34 @@ class MCCA:
         c = c+R
         return c
     #for calculating covariance matrix 
-    def cov_mat():
+    def cov_mat(self,X_list):
+        views = len(X_list)
+        N = len(X_list[0])
         C = [[np.array([]) for i in range(views)] for j in range(views)]
+        
         for i in range(views):
             for j in range(views):
-            C[i][j]=np.dot(X_list[i].T,X_list[j])
-            C[i][j]=np.divide(C[i][j],float(N))
-            if i==j:
-                C[i][j]=self.add_reg_param(C[i][j])
+                C[i][j]=np.dot(X_list[i].T,X_list[j])
+                C[i][j]=np.divide(C[i][j],float(N))
+                if i==j:
+                    C[i][j]=self.add_reg_param(C[i][j])
         
         self.C = C
         return C
 
 
-   def fit(self,X_list):
+    def fit(self,X_list):
         views = len(X_list)
         #normalize the data
         X_list = [self.normalize(x) for x in X_list]
-
+        #calculating covariance 
+        C = self.cov_mat(X_list)
+        
+        dimen = self.dimentions(X_list)
         #create the initial alpha
         alpha_initial = [np.array([[]]) for i in range(views)]
         for k in range(views):
-            alpha_initial[k]=np.random.rand(self.dimen[k])
+            alpha_initial[k]=np.random.rand(dimen[k])
 
         #inialize alpha
         alpha = [[np.array([]) for i in range(views)] for j in range(self.n_components)]
@@ -61,7 +68,7 @@ class MCCA:
         #Horst Algorithm 
         for i in range(self.n_components):
             for j in range(views):
-                sum = np.zeros(self.dimen[j])
+                sum = np.zeros(dimen[j])
                 if i==0:
                     for k in range(views):
                         sum = np.add(sum.T,np.dot(C[j][k],alpha_initial[k].T))
@@ -84,7 +91,7 @@ class MCCA:
 
         self.weights=weights
    
-   def transform(X_list):
+    def transform(self,X_list):
         views = len(X_list)
         X_list = [self.normalize(x) for x in X_list]
         X_reduced = [[]]*views
@@ -99,14 +106,3 @@ class MCCA:
         self.fit(X_list)
         X_reduced.self.tranform(X_list)
         return X_reduced
-                       
-                       
-
-a = np.random.rand(5,5)
-b = np.random.rand(5,6)
-c = np.random.rand(5,7)
-d = np.random.rand(5,8)
-mcca = MCCA()
-mcca.fit(a,b,c,d)
-res=mcca.transform(a,b,c,d)
-print(res)  
