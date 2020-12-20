b169a6844651ae2c4a6a6f21f3924d0120a51e7f
tanishq719 <47773364+tanishq719@users.noreply.github.com>
2020-12-20 21:06:44 +0530

    Update example.py

--- Groups/Group_ID_4/Resources/example.py
+++ Groups/Group_ID_4/Resources/example.py
@@ -1,10 +1,11 @@
-from sklearn.datasets import load_iris
 from RMvFS import RMvFS
 
-r = RMvFS(threshold=0.5)
+r = RMvFS(threshold=0.3,max_iter=14)
 X,y = load_iris(return_X_y=True)
-print("shape of X:")
-print(X.shape)
-r.fit(X,Y=y)
-print(r.transform(X))
+X1 = [[60, 468], [40, 400], [46, 109], [50, 150], [12, 57]]     #height and width of advertisment images
+X2 = [[1, 1, 0, 0, 0], [1, 1, 1, 0, 0], [0, 0, 0, 1, 1], [1, 1, 0, 0, 0], [1, 0, 0, 1, 1]]    # sparse matrix for various tags in the url of asvertisement
+r.fit(X1,X2,Y=['ad', 'ad', 'notad', 'ad', 'notad'])
+
+print(r.transform(X1,X2))
+
 print(r.feature_importance)
